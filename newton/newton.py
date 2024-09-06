9156a9a40990885867544e3d8abdc4a5021dc900
Antônio Fernandes <devantonio.fer@gmail.com>
2024-09-06 20:07:05 -0300

    adding newtons method

---
+++ newton/newton.py
@@ -0,0 +1,36 @@
+def MODULO(n):
+    if n < 0:
+        return n * -1
+    return n
+
+
+def formula(x):
+    return x - (FUNCAO(x)/DERIVADA_FUNCAO(x))
+
+
+def newton(x):
+    if MODULO(FUNCAO(x)) < PRECISAO:
+        return x
+
+    print(x, MODULO(FUNCAO(x+1)))
+    return newton(formula(x))
+    
+
+
+# Precisao
+PRECISAO = 10 ** (-3)
+
+# Chute
+CHUTE = 5
+
+# Funcao
+def FUNCAO(x):
+    return 2 * x - x ** 2
+
+# Derivada da funcao
+def DERIVADA_FUNCAO(x):
+    return 2 - (2 * x)
+
+
+print(newton(CHUTE))
+
